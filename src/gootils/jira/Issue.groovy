d55b23c60c3d7fd998976b3127231dde9433e7f9
Jan Safarik <safarikjt@gmail.com>
2026-02-06 11:35:09 +0100

    [Jira] Add Jira and Issue classes

--- src/gootils/jira/Issue.groovy
+++ src/gootils/jira/Issue.groovy
@@ -1,4 +1,30 @@
 package gootils.jira
 
+import java.time.OffsetDateTime
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
 class Issue {
+	String summary
+	String key
+	String creator
+	OffsetDateTime resolutionDate
+	String priority
+	String resolution
+	String status
+	String type
+
+	Issue(String summary, String key, String creator, String resolutionDate, String priority, String resolution, String status, String type) {
+		this.summary = summary
+		this.key = key
+		this.creator = creator
+		if (resolutionDate) {
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+			this.resolutionDate = OffsetDateTime.parse(resolutionDate, formatter)
+		}
+		this.priority = priority
+		this.resolution = resolution
+		this.status = status
+		this.type = type
+	}
 }
--- src/gootils/jira/Jira.groovy
+++ src/gootils/jira/Jira.groovy
@@ -1,4 +1,59 @@
 package gootils.jira
 
+import gootils.util.Json
+import gootils.util.Shell
+
+import java.nio.charset.StandardCharsets
+
 class Jira {
+	String server
+	String token
+
+	Jira(String server, String token) {
+		this.server = server
+		this.token = token
+	}
+
+	def get(String path) {
+		return Shell.sh([
+			"curl",
+			"--silent",
+			"--request", "GET",
+			"--url", "${server}/rest/api/2${path}",
+			"--header", "Authorization: Bearer ${token}",
+			"--header", "Content-Type:application/json"
+		], false)
+	}
+
+	def getAllJirasOfUser(String username) {
+		int i = 0
+		def issues = []
+		while (true) {
+			def response = Json.readJson(
+				this.get("/search?jql=" +
+					URLEncoder.encode("assignee = \"${username}\" ORDER BY updated DESC", StandardCharsets.UTF_8) +
+					"&startAt=${i}&maxResults=100&fields=key,priority,summary,issuetype,status,creator,resolutiondate")
+			)
+
+			if (response.issues.size() == 0) {
+				break
+			}
+
+			issues.addAll(response.issues.collect {
+				return new Issue(
+					it.fields.summary,
+					it.key,
+					it.fields.creator.emailAddress,
+					it.fields.resolutiondate,
+					it.fields.priority.name,
+					it.fields.status.statusCategory.name,
+					it.fields.status.name,
+					it.fields.issuetype.name
+				)
+			})
+
+			i += 100;
+		}
+		return issues
+	}
 }
